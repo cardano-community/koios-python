84b2d5e796312c33343ab323c70dd1f424972da2
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 10:44:51 +0200

    main

--- tests.py
+++ tests.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
+#from koios_python import block, epochs #alternative if we just need some functions
 import koios_python
 
-
 # con la creacion de diccionarios es fácil la impresion de un campo
-params = epochs.get_epoch_params(336)
+params = koios_python.get_epoch_params(336)
 nonce = params.get("nonce")
 print(f"Nonce: {nonce}")
 
-print(block.get_block_info("b39e3463fb83c547c2410abb39d92270dee5cda7417b10755303326df09aac86"))
+print(koios_python.get_block_info("b39e3463fb83c547c2410abb39d92270dee5cda7417b10755303326df09aac86"))
