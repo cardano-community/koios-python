453bf8bfb0e0eedc7456c0b884b93507576b6271
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 09:58:15 +0200

    Create transactions.py

---
+++ transactions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import json
+import requests
+
+
+def get_tx(tx_hash):
+    """Get Tx data"""
+    tx = {"_tx_hashes":[tx_hash]}
+    koios_post = requests.post( "https://api.koios.rest/api/v0/tx_info", json = tx)
+    koios_post = json.loads(koios_post.content)[0]
+    print(koios_post)
