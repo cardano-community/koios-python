90fd915c7db3d6d846eee41a449e812ba8660f5b
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 10:29:40 +0200

    main

--- koios_python/__init__.py
+++ koios_python/__init__.py
@@ -1,1 +1,4 @@
-
+from .block import *
+from .epochs import *
+from .network import *
+from .transactions import *
--- tests.py
+++ tests.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import json
-import requests
+import koios_python
 
 
 # con la creacion de diccionarios es fácil la impresion de un campo
-params = get_epoch_params(336)
+params = epochs.get_epoch_params(336)
 nonce = params.get("nonce")
 print(f"Nonce: {nonce}")
 
-print(get_block_info("b39e3463fb83c547c2410abb39d92270dee5cda7417b10755303326df09aac86").get("epoch_slot"))
+print(block.get_block_info("b39e3463fb83c547c2410abb39d92270dee5cda7417b10755303326df09aac86"))
