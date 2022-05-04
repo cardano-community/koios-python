fb12bbf778c8ff6cbc4f997191adf645309124ee
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 10:36:42 +0200

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import koios_python
+
+
+# con la creacion de diccionarios es fácil la impresion de un campo
+params = epochs.get_epoch_params(336)
+nonce = params.get("nonce")
+print(f"Nonce: {nonce}")
+
+print(block.get_block_info("b39e3463fb83c547c2410abb39d92270dee5cda7417b10755303326df09aac86"))
