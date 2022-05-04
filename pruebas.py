445066f87f7ecc96ad992c6da205a9be4495ffea
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 09:52:20 +0200

    main

--- pruebas.py
+++ pruebas.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
 import json
 import requests
 
 ########### NETWORK ############################
 def get_tip():
-    """Get the tip info about the latest block seen by chain"""
+    """
+    Get the tip info about the latest block seen by chain
+    :return: Blockchain tip data
+    """
     tip = requests.get("https://api.koios.rest/api/v0/tip")
     #confirmar que es correcto codigo 200
     #tip = tip.json
@@ -12,15 +17,22 @@ def get_tip():
 
 
 def get_genesis():
-    """Get the Genesis parameters used to start specific era on chain"""
+    """
+    Get the Genesis parameters used to start specific era on chain
+    :return: Genesis block data
+    """
     genesis = requests.get("https://api.koios.rest/api/v0/genesis")
     genesis = json.loads(genesis.content)
     return genesis
 
 
 def get_totals(epoch_no):
-    """Get the circulating utxo, treasury, rewards, supply and reserves in lovelace for specified \
-    epoch, all epochs if empty"""
+    """
+    Get the circulating utxo, treasury, rewards, supply and reserves in lovelace for specified \
+    epoch, all epochs if empty
+     :param stake_address: The stake addresse
+    :return: Blockchain tip data
+    """
     totals = requests.get("https://api.koios.rest/api/v0/totals?_epoch_no="+str(epoch_no))
     totals = json.loads(totals.content)[0]
     return totals
