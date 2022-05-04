3c84044cf2b933407426fc1b606325e5984d84a5
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 14:14:10 +0200

    main

--- koios_python/transactions.py
+++ koios_python/transactions.py
@@ -4,13 +4,13 @@ import json
 import requests
 
 
-def get_tx(tx_hash):
+def get_tx_info(tx_hash):
     """
-    Get Tx data
-    params:
-    return: 
+    Get detailed information about transaction(s)
+    params: transaction hash to search and read data
+    return: all info about transaction(s)
     """
-    tx = {"_tx_hashes":[tx_hash]}
-    koios_post = requests.post( "https://api.koios.rest/api/v0/tx_info", json = tx)
+    tx_hash = {"_tx_hashes":[tx_hash]}
+    tx_info = requests.post( "https://api.koios.rest/api/v0/tx_info", json = tx_hash)
     koios_post = json.loads(koios_post.content)[0]
-    print(koios_post)
+    return tx_info
