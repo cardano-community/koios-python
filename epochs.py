fcfd414de3869b2073174dbb069c8f84b2949f61
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-04 09:56:23 +0200

    Create epochs.py

---
+++ epochs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+import requests
+
+
+def get_epoch_info(epoch_no):
+    """Get the epoch information, all epochs if no epoch specified"""
+    info = requests.get("https://api.koios.rest/api/v0/epoch_info?_epoch_no="+str(epoch_no))
+    info = json.loads(info.content)[0]
+    return info
+
+
+def get_epoch_params(epoch_no):
+    """Get the protocol parameters for specific epoch, returns information about all epochs \
+    if no epoch specified"""
+    info = requests.get("https://api.koios.rest/api/v0/epoch_params?_epoch_no="+str(epoch_no))
+    info = json.loads(info.content)[0]
+    return info
