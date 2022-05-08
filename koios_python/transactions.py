a5b32a79446b8dcb05b67f8ff2693d70a13e0326
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2022-05-08 21:45:54 +0200

    main

--- koios_python/transactions.py
+++ koios_python/transactions.py
@@ -12,7 +12,7 @@ def get_tx_info(tx_hash):
     :return: list of all info about transaction(s).
     :rtype: list.
     """
-    get_format = {"_tx_hashes":tx_hash}
+    get_format = {"_tx_hashes": [tx_hash]}
     tx_info = requests.post( "https://api.koios.rest/api/v0/tx_info", json = get_format)
     tx_info  = json.loads(tx_info.content)
     return tx_info
@@ -26,7 +26,7 @@ def get_tx_utxos(tx_hash):
     :return: all info about utxos in transaction(s)
     :rtype: list.
     """
-    get_format = {"_tx_hashes":tx_hash}
+    get_format = {"_tx_hashes": [tx_hash]}
     tx_utxos = requests.post("https://api.koios.rest/api/v0/tx_utxos", json = get_format)
     tx_utxos  = json.loads(tx_utxos.content)
     return tx_utxos
@@ -36,11 +36,11 @@ def get_tx_metadata(tx_hash):
     """
     Get metadata information (if any) for given transaction(s).
 
-    :param list tx_hash: list of transaction(s) hash to search and read utxos data.
+    :param list tx_hash: list strings of transaction(s) hash to search and read utxos data.
     :return: list of all info about utxos in transaction(s)
     :rtype: list.
     """
-    get_format = {"_tx_hashes":tx_hash}
+    get_format = {"_tx_hashes": [tx_hash]}
     tx_metadata = requests.post("https://api.koios.rest/api/v0/tx_metadata", json = get_format)
     tx_metadata  = json.loads(tx_metadata.content)
     return tx_metadata
@@ -82,7 +82,7 @@ def get_tx_status(tx_hash):
     :return: list of all info about utxos in transaction(s)
     :rtype: list.
     """
-    get_format = {"_tx_hashes":tx_hash}
+    get_format = {"_tx_hashes": [tx_hash]}
     tx_status = requests.post( "https://api.koios.rest/api/v0/tx_status", json = get_format)
     tx_status  = json.loads(tx_status.content)
     return tx_status
--- tests.py
+++ tests.py
@@ -12,22 +12,26 @@ import koios_python
 #print(koios_python.get_epoch_params(338))
 
 # Get info of two transactions:
-#print(koios_python.get_tx_metadata(["f144a8264acf4bdfe2e1241170969c930d64ab6b0996a4a45237b623f1dd670e","0b8ba3bed976fa4913f19adc9f6dd9063138db5b4dd29cecde369456b5155e94"]))
+#print(koios_python.get_tx_metadata(["f144a8264acf4bdfe2e1241170969c930d64ab6b0996a4a45237b623f1dd670e" \
+# ,"0b8ba3bed976fa4913f19adc9f6dd9063138db5b4dd29cecde369456b5155e94"]))
 
 # Submit an already serialized transaction to the network from a binay file-system
 #print(koios_python.submit_tx("signed.cbor"))
 
 # Get number of confirmations of a list of hash transactions
-#print(koios_python.get_tx_status(["b50d56706c07a3c11f21fbbca3da7a3754398acb38b91a38a36cbea5c895e02f","0b8ba3bed976fa4913f19adc9f6dd9063138db5b4dd29cecde369456b5155e94"]))
+#print(koios_python.get_tx_status(["b50d56706c07a3c11f21fbbca3da7a3754398acb38b91a38a36cbea5c895e02f" \
+# ,"0b8ba3bed976fa4913f19adc9f6dd9063138db5b4dd29cecde369456b5155e94"]))
 
 # Get address info - balance, associated stake address (if any) and UTxO set.
 #print(koios_python.get_address_info("addr1qyp9kz50sh9c53hpmk3l4ewj9ur794t2hdqpngsjn3wkc5sztv9glpwt3frwrhdrltjaytc8ut2k4w6qrx3p98zad3fq07xe9g"))
 
 # Get the full rewards history (including MIR) for a stake address, or certain epoch if specified
-#print(koios_python.get_account_rewards("stake1u8jm3v2q8h46q485j8e8uxqmj33f4cy4xvadkuq5g2c27ls44jflg",335))
+#print(koios_python.get_account_rewards("stake1u8jm3v2q8h46q485j8e8uxqmj33f4cy4xvadkuq5g2c27ls44jflg" \
+# ,335))
 
 # Get the mint/burn history of an asset
-#print(koios_python.get_asset_history("d3501d9531fcc25e3ca4b6429318c2cc374dbdbcf5e99c1c1e5da1ff","444f4e545350414d"))
+#print(koios_python.get_asset_history("d3501d9531fcc25e3ca4b6429318c2cc374dbdbcf5e99c1c1e5da1ff" \
+# , "444f4e545350414d"))
 
 # Current pool statuses and details for a specified list of pool ids
 #print(koios_python.get_pool_info(["pool100wj94uzf54vup2hdzk0afng4dhjaqggt7j434mtgm8v2gfvfgp", \
