36505ee0b746a5347a4727128d929635ec4935e5
Wael <wael.t.ivie@gmail.com>
2022-10-18 19:28:20 -0700

    working on new features

--- koios_python/epoch.py
+++ koios_python/epoch.py
@@ -4,10 +4,11 @@ Provides all epoch functions
 """
 import json
 import requests
-from .urls import EPOCH_INFO_URL, EPOCH_PARAMS_URL 
+import urls as u
 
+# from .urls import EPOCH_INFO_URL, EPOCH_PARAMS_URL 
 
-def get_epoch_info(epoch_no=None):
+def get_epoch_info(url,epoch_no=None):
     """
     Get the epoch information, all epochs if no epoch specified.
 
@@ -15,16 +16,20 @@ def get_epoch_info(epoch_no=None):
     :return: list of detailed summary for each epoch.
     :rtype: list
     """
+    
+    
     if epoch_no is None:
-        info = requests.get(EPOCH_INFO_URL, timeout=10)
+        info = requests.get(url, timeout=10)
         info = json.loads(info.content)
+        print(info)
     else:
-        info = requests.get(EPOCH_INFO_URL + "?_epoch_no=" + str(epoch_no), timeout=10)
+        info = requests.get(url + "?_epoch_no=" + str(epoch_no), timeout=10)
         info = json.loads(info.content)
+        print(info)
     return info
 
 
-def get_epoch_params(epoch_no=None):
+def get_epoch_params(url,epoch_no=None):
     """
     Get the protocol parameters for specific epoch, returns information about all epochs
     if no epoch specified.
@@ -34,9 +39,9 @@ def get_epoch_params(epoch_no=None):
     :rtype: list
     """
     if epoch_no is None:
-        info = requests.get(EPOCH_PARAMS_URL, timeout=10)
+        info = requests.get(url, timeout=10)
         info = json.loads(info.content)
     else:
-        info = requests.get(EPOCH_PARAMS_URL + "?_epoch_no=" + str(epoch_no), timeout=10)
+        info = requests.get(url + "?_epoch_no=" + str(epoch_no), timeout=10)
         info = json.loads(info.content)
     return info
--- koios_python/urls.py
+++ koios_python/urls.py
@@ -3,68 +3,96 @@
 Provides all urls used in the library
 """
 
-# Koios Network API URL
-KOIOS_URL = "https://api.koios.rest/api/v0/"
+class URLs:
+        
+        def __init__(self, url='https://api.koios.rest/api/v0/', network=None):
+                self.url = url
+                self.network = network
+                
+                if self.network != None:
+                        # replace any subdomain with "network" subdomain
+                        self.url = self.url.replace(self.url.split('.')[0], self.network)
+                        
+                        
+        # EPOCH URLs
+        def epoch_info_url(self):
+                
+                return self.url + "epoch_info"
+        
+        def epoch_params_url(self):
+                return self.url + "epoch_params"
+        
+        # Network URLs
+        def tip_url(self):
+                return self.url + "tip"
+        
+        def genesis_url(self):
+                return self.url + "genesis"
+        
+        def totals_url(self):
+                return self.url + "totals"
+        
+        # Block URLs
 
-# Network URLs
-TIP_URL = KOIOS_URL + "tip"
-GENESIS_URL = KOIOS_URL + "genesis"
-TOTALS_URL = KOIOS_URL + "totals"
+# # Network URLs
+# TIP_URL = KOIOS_URL + "tip"
+# GENESIS_URL = KOIOS_URL + "genesis"
+# TOTALS_URL = KOIOS_URL + "totals"
 
 # Epoch URLs
-EPOCH_INFO_URL = KOIOS_URL + "epoch_info"
-EPOCH_PARAMS_URL = KOIOS_URL + "epoch_params"
+# EPOCH_INFO_URL = KOIOS_URL + "epoch_info"
+# EPOCH_PARAMS_URL = KOIOS_URL + "epoch_params"
 
-# Block URLs
-BLOCKS_URL = KOIOS_URL + "blocks"
-BLOCK_INFO_URL = KOIOS_URL + "block_info"
-BLOCK_TXS_URL = KOIOS_URL + "block_txs"
+# # Block URLs
+# BLOCKS_URL = KOIOS_URL + "blocks"
+# BLOCK_INFO_URL = KOIOS_URL + "block_info"
+# BLOCK_TXS_URL = KOIOS_URL + "block_txs"
 
-# Transactions URLs
-TX_INFO_URL = KOIOS_URL + "tx_info"
-TX_UTXOS_URL = KOIOS_URL + "tx_utxos"
-TX_METADATA_URL = KOIOS_URL + "tx_metadata"
-TX_METALABELS_URL = KOIOS_URL + "tx_metalabels"
-SUBMIT_TX_URL = KOIOS_URL + "submittx"
-TX_STATUS_URL = KOIOS_URL + "tx_status"
+# # Transactions URLs
+# TX_INFO_URL = KOIOS_URL + "tx_info"
+# TX_UTXOS_URL = KOIOS_URL + "tx_utxos"
+# TX_METADATA_URL = KOIOS_URL + "tx_metadata"
+# TX_METALABELS_URL = KOIOS_URL + "tx_metalabels"
+# SUBMIT_TX_URL = KOIOS_URL + "submittx"
+# TX_STATUS_URL = KOIOS_URL + "tx_status"
 
-# Address URLs
-ADDRESS_INFO_URL = KOIOS_URL + "address_info"
-ADDRESS_TXS_URL = KOIOS_URL + "address_txs"
-ADDRESS_ASSETS_URL = KOIOS_URL + "address_assets"
-CREDENTIAL_TXS_URL = KOIOS_URL + "credential_txs"
+# # Address URLs
+# ADDRESS_INFO_URL = KOIOS_URL + "address_info"
+# ADDRESS_TXS_URL = KOIOS_URL + "address_txs"
+# ADDRESS_ASSETS_URL = KOIOS_URL + "address_assets"
+# CREDENTIAL_TXS_URL = KOIOS_URL + "credential_txs"
 
-# Account URLs
-ACCOUNT_LIST_URL = KOIOS_URL + "account_list"
-ACCOUNT_INFO_URL = KOIOS_URL + "account_info"
-ACCOUNT_REWARDS_URL = KOIOS_URL + "account_rewards"
-ACCOUNT_UPDATES_URL = KOIOS_URL + "account_updates"
-ACCOUNT_ADDRESES_URL = KOIOS_URL + "account_addresses"
-ACCOUNT_ASSETS_URL = KOIOS_URL + "account_assets"
-ACCOUNT_HISTORY_URL = KOIOS_URL + "account_history"
+# # Account URLs
+# ACCOUNT_LIST_URL = KOIOS_URL + "account_list"
+# ACCOUNT_INFO_URL = KOIOS_URL + "account_info"
+# ACCOUNT_REWARDS_URL = KOIOS_URL + "account_rewards"
+# ACCOUNT_UPDATES_URL = KOIOS_URL + "account_updates"
+# ACCOUNT_ADDRESES_URL = KOIOS_URL + "account_addresses"
+# ACCOUNT_ASSETS_URL = KOIOS_URL + "account_assets"
+# ACCOUNT_HISTORY_URL = KOIOS_URL + "account_history"
 
-# Asset URLs
-ASSET_LIST_URL = KOIOS_URL + "asset_list"
-ASSET_ADDRESS_LIST_URL = KOIOS_URL +  "asset_address_list?_asset_policy="
-ASSET_INFO_URL = KOIOS_URL + "asset_info?_asset_policy="
-ASSET_HISTORY_URL = KOIOS_URL + "asset_history?_asset_policy="
-ASSET_POLICY_INFO_URL = KOIOS_URL + "asset_policy_info?_asset_policy="
-ASSET_SUMMARY_URL = KOIOS_URL + "asset_summary?_asset_policy="
-ASSET_TXS_URL = KOIOS_URL + "asset_txs?_asset_policy="
+# # Asset URLs
+# ASSET_LIST_URL = KOIOS_URL + "asset_list"
+# ASSET_ADDRESS_LIST_URL = KOIOS_URL +  "asset_address_list?_asset_policy="
+# ASSET_INFO_URL = KOIOS_URL + "asset_info?_asset_policy="
+# ASSET_HISTORY_URL = KOIOS_URL + "asset_history?_asset_policy="
+# ASSET_POLICY_INFO_URL = KOIOS_URL + "asset_policy_info?_asset_policy="
+# ASSET_SUMMARY_URL = KOIOS_URL + "asset_summary?_asset_policy="
+# ASSET_TXS_URL = KOIOS_URL + "asset_txs?_asset_policy="
 
-# Pool URLs
-POOL_LIST_URL = KOIOS_URL + "pool_list"
-POOL_INFO_URL = KOIOS_URL + "pool_info"
-POOL_STAKE_SNAPSHOT = KOIOS_URL + "pool_stake_snapshot?_pool_bech32="
-POOL_DELEGATORS_URL = KOIOS_URL + "pool_delegators?_pool_bech32="
-POOL_DELEGATORS_HISTORY_URL = KOIOS_URL + "pool_delegators_history?_pool_bech32="
-POOL_BLOCKS_URL = KOIOS_URL + "pool_blocks?_pool_bech32="
-POOL_HISTORY_URL = KOIOS_URL + "pool_history?_pool_bech32="
-POOL_UPDATES_URL = KOIOS_URL + "pool_updates"
-POOL_RELAYS_URL = KOIOS_URL + "pool_relays"
-POOL_METADATA_URL = KOIOS_URL + "pool_metadata"
+# # Pool URLs
+# POOL_LIST_URL = KOIOS_URL + "pool_list"
+# POOL_INFO_URL = KOIOS_URL + "pool_info"
+# POOL_STAKE_SNAPSHOT = KOIOS_URL + "pool_stake_snapshot?_pool_bech32="
+# POOL_DELEGATORS_URL = KOIOS_URL + "pool_delegators?_pool_bech32="
+# POOL_DELEGATORS_HISTORY_URL = KOIOS_URL + "pool_delegators_history?_pool_bech32="
+# POOL_BLOCKS_URL = KOIOS_URL + "pool_blocks?_pool_bech32="
+# POOL_HISTORY_URL = KOIOS_URL + "pool_history?_pool_bech32="
+# POOL_UPDATES_URL = KOIOS_URL + "pool_updates"
+# POOL_RELAYS_URL = KOIOS_URL + "pool_relays"
+# POOL_METADATA_URL = KOIOS_URL + "pool_metadata"
 
-# Scripts URLs
-NATIVE_SCRIPT_LIST_URL = KOIOS_URL + "native_script_list"
-PLUTUS_SCRIPT_LIST_URL = KOIOS_URL + "plutus_script_list"
-SCRIPT_REDEEMERS_URL = KOIOS_URL + "script_redeemers"
+# # Scripts URLs
+# NATIVE_SCRIPT_LIST_URL = KOIOS_URL + "native_script_list"
+# PLUTUS_SCRIPT_LIST_URL = KOIOS_URL + "plutus_script_list"
+# SCRIPT_REDEEMERS_URL = KOIOS_URL + "script_redeemers"
