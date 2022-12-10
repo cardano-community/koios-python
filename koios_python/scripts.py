21a5cf390061f3c80b8398005fb7155aed50d0c5
Quixote <quixotepool@proton.me>
2022-12-10 16:35:48 +0100

    main

--- koios_python/scripts.py
+++ koios_python/scripts.py
@@ -45,3 +45,16 @@ def get_script_redeemers(self, script_hash):
     query = requests.get(self.SCRIPT_REDEEMERS_URL + script_hash, timeout=30)
     query  = json.loads(query.content)
     return query
+
+def get_datum_info(self, *datum_hash):
+    '''
+    Get list of datum information for given datum hashes
+
+    :params string datum_hash: The Hash of the Plutus Data.
+    :return: the actual data in json form.
+    :rtype: list.
+    '''
+    custom_headers = {"_datum_hashes": [datum_hash]}
+    get_format = requests.post(self.DATUM_INFO_URL, json = custom_headers, timeout=30)
+    get_format = json.loads(get_format.content)
+    return get_format
