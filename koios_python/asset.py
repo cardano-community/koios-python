789345d808d306a4655bf05ac1bb40ca7da48fde
Wael <wael.t.ivie@gmail.com>
2023-01-05 14:46:57 -0800

    assets.py update

--- koios_python/asset.py
+++ koios_python/asset.py
@@ -4,10 +4,23 @@ Provides all asset functions
 """
 import json
 import requests
-from .environment import BASE_TIMEOUT, LIMIT_TIMEOUT
+from .environment import *
 
-
+@Exception_Handler
 def get_asset_list(self, content_range="0-999"):
+    """
+    Get the list of all native assets (paginated)
+
+    :return: list with all asset list.
+    :rtype: list.
+    """
+    custom_headers = {"Range": str(content_range)}
+    asset_list = requests.get(self.ASSET_LIST_URL, headers = custom_headers)
+    asset_list = json.loads(asset_list.content)
+    return asset_list
+
+
+# def get_asset_list(self, content_range="0-999"):
     """
     Get the list of all native assets (paginated)
 
@@ -34,8 +47,23 @@ def get_asset_list(self, content_range="0-999"):
 
     return asset_list
 
-
+@Exception_Handler
 def get_asset_address_list(self, asset_policy, asset_name, content_range="0-999"):
+    """
+    Get the list of all addresses holding a given asset.
+
+    :param str asset_policy: asset Policy ID in hexadecimal format (hex).
+    :param str asset_name: string with Asset Name in hexadecimal format (hex).
+    :return: list of all addresses.
+    :rtype: list.
+    """
+    custom_headers = {"Range": str(content_range)}
+    info = requests.get(f"{self.ASSET_ADDRESS_LIST_URL}{asset_policy}&_asset_name={asset_name}", \
+        headers = custom_headers)
+    info = json.loads(info.content)
+    return info
+
+# def get_asset_address_list(self, asset_policy, asset_name, content_range="0-999"):
     """
     Get the list of all addresses holding a given asset.
 
@@ -65,8 +93,21 @@ def get_asset_address_list(self, asset_policy, asset_name, content_range="0-999"
 
     return info
 
-
+@Exception_Handler
 def get_asset_info(self, asset_policy, asset_name):
+    """
+    Get the information of an asset including first minting & token registry metadata.
+
+    :param str asset_policy: asset Policy ID in hexadecimal format (hex).
+    :param str asset_name: string with Asset Name in hexadecimal format (hex).
+    :return: list of all asset info.
+    :rtype: list.
+    """
+    info = requests.get(f"{self.ASSET_INFO_URL}{asset_policy}&_asset_name={asset_name}")
+    info = json.loads(info.content)
+    return info
+
+# def get_asset_info(self, asset_policy, asset_name):
     """
     Get the information of an asset including first minting & token registry metadata.
 
@@ -94,8 +135,22 @@ def get_asset_info(self, asset_policy, asset_name):
 
     return info
 
-
+@Exception_Handler
 def get_asset_history(self, asset_policy, asset_name):
+    """
+    Get the mint/burn history of an asset.
+
+    :param str asset_policy: asset Policy ID in hexadecimal format (hex).
+    :param str asset_name: string with Asset Name in hexadecimal format (hex).
+    :return: list of asset mint/burn history.
+    :rtype: list.
+    """
+    history = requests.get(f"{self.ASSET_HISTORY_URL}{asset_policy}&_asset_name={asset_name}")
+    history = json.loads(history.content)
+    return history
+
+
+# def get_asset_history(self, asset_policy, asset_name):
     """
     Get the mint/burn history of an asset.
 
@@ -123,8 +178,20 @@ def get_asset_history(self, asset_policy, asset_name):
 
     return history
 
-
+@Exception_Handler
 def get_asset_policy_info(self, asset_policy):
+    """
+    Get the information for all assets under the same policy.
+
+    :param str asset_policy: asset Policy ID in hexadecimal format (hex).
+    :return: list of all mint/burn transactions for an asset
+    :rtype: list.
+    """
+    info = requests.get(f"{self.ASSET_POLICY_INFO_URL}{asset_policy}")
+    info = json.loads(info.content)
+    return info
+
+# def get_asset_policy_info(self, asset_policy):
     """
     Get the information for all assets under the same policy.
 
@@ -150,8 +217,22 @@ def get_asset_policy_info(self, asset_policy):
 
     return info
 
-
+@Exception_Handler
 def get_asset_summary(self, asset_policy, asset_name):
+    """
+    Get the summary of an asset (total transactions exclude minting/total wallets include only
+    wallets with asset balance).
+
+    :param str asset_policy: asset Policy ID in hexadecimal format (hex).
+    :param str asset_name: string with Asset Name in hexadecimal format (hex).
+    :return: list of asset summary information.
+    :rtype: list.
+    """
+    summary = requests.get(f"{self.ASSET_SUMMARY_URL}{asset_policy}&_asset_name={asset_name}")
+    summary = json.loads(summary.content)
+    return summary
+
+# def get_asset_summary(self, asset_policy, asset_name):
     """
     Get the summary of an asset (total transactions exclude minting/total wallets include only
     wallets with asset balance).
@@ -180,8 +261,22 @@ def get_asset_summary(self, asset_policy, asset_name):
 
     return summary
 
-
+@Exception_Handler
 def get_asset_txs(self, asset_policy, asset_name, after_block_height=0):
+    """
+    Get the list of all asset transaction hashes (newest first).
+
+    :param str asset_policy: asset Policy ID in hexadecimal format (hex).
+    :param str asset_name: string with Asset Name in hexadecimal format (hex).
+    :param int after_block_height: Block height for specifying time delta, if not data start from 0
+    :return: list of all asset hashes transactions.
+    :rtype: list.
+    """
+    txs = requests.get(f"{self.ASSET_TXS_URL}{asset_policy}&_asset_name={asset_name}&_after_block_height={after_block_height}")
+    txs = json.loads(txs.content)
+    return txs
+
+# def get_asset_txs(self, asset_policy, asset_name, after_block_height=0):
     """
     Get the list of all asset transaction hashes (newest first).
 
