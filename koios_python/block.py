542af7bf62c9d573e68657dac2effc8c3268e4f3
Wael <wael.t.ivie@gmail.com>
2023-01-05 14:07:15 -0800

    blocks.py update

--- koios_python/block.py
+++ koios_python/block.py
@@ -4,9 +4,22 @@ Provides all block functions
 """
 import json
 import requests
-from .environment import BASE_TIMEOUT, LIMIT_TIMEOUT
+from .environment import *
 
+@Exception_Handler
 def get_blocks(self,content_range="0-999"):
+    """
+    Get summarised details about all blocks (paginated - latest first).
+
+    :param str range: paginated content range, up to  1000 records.
+    :return: list of all blocks.
+    :rtype: list
+    """
+    blocks = requests.get(self.BLOCKS_URL)
+    blocks = json.loads(blocks.content)
+    return blocks 
+
+# def get_blocks(self,content_range="0-999"):
     """
     Get summarised details about all blocks (paginated - latest first).
 
@@ -34,8 +47,21 @@ def get_blocks(self,content_range="0-999"):
 
     return blocks
 
-
+@Exception_Handler
 def get_block_info(self,*block_hash):
+    """
+    Get detailed information about a specific block or blocks
+
+    :param str block_hash: block/s hash ID.
+    :return:  list of detailed block information.
+    :rtype: list
+    """
+    get_format = {"_block_hashes":[block_hash]}
+    block = requests.post(self.BLOCK_INFO_URL, json = get_format)
+    block = json.loads(block.content)
+    return block
+
+# def get_block_info(self,*block_hash):
     """
     Get detailed information about a specific block or blocks
 
@@ -63,8 +89,21 @@ def get_block_info(self,*block_hash):
 
     return block
 
-
+@Exception_Handler
 def get_block_txs(self,*block_hash):
+    """
+    Get a list of all transactions included in a provided block.
+
+    :param str block_hash: block hash ID.
+    :return: list of transactions hashes.
+    :rtype: list
+    """
+    get_format = {"_block_hashes":[block_hash]}
+    txs = requests.post(self.BLOCK_TXS_URL, json = get_format)
+    txs = json.loads(txs.content)
+    return txs
+
+# def get_block_txs(self,*block_hash):
     """
     Get a list of all transactions included in a provided block.
 
